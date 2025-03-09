f7a7276e5b5fb67c9b780eb6cdedf712b43bbcad
Amir-dev01 <saliharamir1@gmail.com>
2025-03-09 18:43:56 +0600

    hw2

--- books/models.py
+++ books/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Books(models.Model):
+    GENRE = (
+        ('Фантастика', 'Фантастика'),
+        ('Роман', 'Роман'),
+        ('Лирика', 'Лирика'),
+    )
+    image = models.ImageField(upload_to='books/')
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    genre = models.CharField(max_length=10, choices=GENRE)
+    mail = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
--- books/urls.py
+++ books/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("about_me/", views.about_me),
     path("domestic/", views.domestic),
-    path("time/", views.time)
+    path("time/", views.time),
+    path('books_list/', views.books_list),
+    path('books_list/<int:id>/', views.books_detail),
 ]
