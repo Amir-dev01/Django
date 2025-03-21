214a1cdbc45824659b631c00a879150ae2886a01
Amir-dev01 <saliharamir1@gmail.com>
2025-03-21 15:56:11 +0600

    hw5

--- hashtags/views.py
+++ hashtags/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import render
 from . import models
+from django.views import generic
 
 
-def all_category_books(request):
-    if request.method == 'GET':
-        query = models.Product.objects.all()
-        return render(request,
-                      template_name='tags/all_category_books.html',
-                      context={'query': query}
-                      )
-def teen_category_books(request):
-    if request.method == 'GET':
-        query = models.Product.objects.all().filter(tags__name='Книги для подростков')
-        return render(request,
-                      template_name='tags/teen_category_books.html',
-                      context={'query': query}
-                      )
-def kids_category_books(request):
-    if request.method == 'GET':
-        query = models.Product.objects.all().filter(tags__name='Книги для детей')
-        return render(request,
-                      template_name='tags/kids_category_books.html',
-                      context={'query': query})
+class AllCategoryBooksView(generic.ListView):
+    model = models.Product
+    template_name = 'tags/all_category_books.html'
+    context_object_name = 'query'
+
+class TeenCategoryBooksView(generic.ListView):
+    model = models.Product
+    template_name = 'tags/teen_category_books.html'
+    context_object_name = 'query'
+
+    def get_queryset(self):
+        return models.Product.objects.filter(tags__name='Книги для подростков')
+
+class KidsCategoryBooksView(generic.ListView):
+    model = models.Product
+    template_name = 'tags/kids_category_books.html'
+    context_object_name = 'query'
+
+    def get_queryset(self):
+        return models.Product.objects.filter(tags__name='Книги для детей')
