b99408b0b102ff4440ad7a794e9d2231160a024a
Amir-dev01 <saliharamir1@gmail.com>
2025-03-27 20:39:06 +0600

    cbv

--- books/urls.py
+++ books/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("about_me/", views.about_me),
-    path("domestic/", views.domestic),
-    path("time/", views.time),
+    path("about_me/", views.AboutMeView.as_view(), name="about_me"),
+    path("domestic/", views.DomesticView.as_view(), name="domestic"),
+    path("time/", views.TimeView.as_view(), name="time"),
     path('', views.BooksListView.as_view(),name='books'),
     path('books_list/<int:id>/', views.BooksDetailView.as_view()),
     path('search/',views.SearchBooksView.as_view(),name='search'),
