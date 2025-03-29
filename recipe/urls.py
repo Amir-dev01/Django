3dd3a77d555e39b157232b376d1900525a479341
Amir-dev01 <saliharamir1@gmail.com>
2025-03-29 21:13:27 +0600

    final_work

--- recipe/urls.py
+++ recipe/urls.py
@@ -3,7 +3,7 @@ from .views import RecipeListView, RecipeDetailView, RecipeCreateView, RecipeDel
 
 
 urlpatterns = [
-    path('', RecipeListView.as_view(), name='recipe_list'),
+    path('recipe/list/', RecipeListView.as_view(), name='recipe_list'),
     path('recipe/<int:pk>/', RecipeDetailView.as_view(), name='recipe_detail'),
     path('recipe/create/', RecipeCreateView.as_view(), name='recipe_create'),
     path('recipe/<int:pk>/delete/', RecipeDeleteView.as_view(), name='recipe_delete'),
